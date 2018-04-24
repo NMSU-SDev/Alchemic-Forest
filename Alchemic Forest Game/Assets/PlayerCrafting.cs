d5067bd85ed917e76d1b5fdd8343678d147c4102
Michael Daviet <Michael@thedaviets.com>
2018-04-23 22:39:02 -0600

    Add handling to remove items from inventory when crafting.

--- Alchemic Forest Game/Assets/PlayerCrafting.cs
+++ Alchemic Forest Game/Assets/PlayerCrafting.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerCrafting : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        GameObject player = GameObject.Find("Player");
-        PlayerMovement inventory = player.GetComponent<PlayerMovement>(); // !!! change me when inventory moves to own script !!!
-	}
+
+    //private GameObject player;// = GameObject.Find("Player");
+    //private PlayerMovement inventory;// = player.GetComponent<PlayerMovement>(); // !!! change me when inventory moves to own script !!!
+
+    // Use this for initialization
+    void Start () {
+        //GameObject player = GameObject.Find("Player");
+        //PlayerMovement inventory = player.GetComponent<PlayerMovement>(); // !!! change me when inventory moves to own script !!!
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,9 +21,17 @@ public class PlayerCrafting : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        GameObject player = GameObject.Find("Player");
+
         if (other.gameObject.CompareTag("Transmutation"))
         {
+            PlayerMovement inventory = player.GetComponent<PlayerMovement>(); // !!! change me when inventory moves to own script !!!
 
+            if (inventory.logCount >= 3)
+            {
+                inventory.logCount -= 3;
+                inventory.logDisplay.text = inventory.logCount.ToString();
+            }
         }//end Transmutation if
     }// end of OnTriggerEnter
 }// end PlayerCrafting
