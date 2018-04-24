4df0322cb0ff102ba9eb59d17a9b423b58f3c045
Michael Daviet <Michael@thedaviets.com>
2018-04-23 21:03:41 -0600

    Add "Transmutation Circle" and empty script for crafting.

---
+++ Alchemic Forest Game/Assets/PlayerCrafting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCrafting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Transmutation"))
+        {
+
+        }//end Transmutation if
+    }// end of OnTriggerEnter
+}// end PlayerCrafting
