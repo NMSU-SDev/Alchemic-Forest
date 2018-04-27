4269880681da9b40e0e2dd2bbef4ac4fff20ad75
Lei Qian <leiqian@nmsu.edu>
2018-04-27 13:28:54 -0600

    04/27/2018 Modified spring-level

---
+++ Alchemic Forest Game/Assets/Scripts/Tfish.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tfish : MonoBehaviour {
+
+    private Rigidbody2D myRigidbody;
+
+    private float movementSpeed; 
+
+	// Use this for initialization
+	void Start () {
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+
+        HandleMovement(horizontal);
+		
+	}
+
+    private void HandleMovement(float horizontal)
+    {
+        myRigidbody.velocity = new Vector2(horizontal, myRigidbody.velocity.y);
+    }
+
+}
