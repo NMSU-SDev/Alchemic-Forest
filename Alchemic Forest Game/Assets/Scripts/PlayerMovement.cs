3f0209428eac4b0b4a00d9fc1a12199abd766583
Michael Daviet <Michael@thedaviets.com>
2018-05-12 01:45:03 -0600

    Resolve conflicting Player movement script.

--- Alchemic Forest Game/Assets/Scripts/PlayerMovement.cs
+++ Alchemic Forest Game/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     private Rigidbody2D rigid2D;
     public int logCount;// dependency in PlayerCrafting.cs
@@ -20,13 +21,20 @@ public class PlayerMovement : MonoBehaviour {
     public int gemCount;
     public Text gemDisplay;
 
-    public Color black = new Color(0, 0, 0);
-    public float fadeSpeed = 2f;
     public int fishCount;
     public Text fishDisplay;
 
     public int torchCount;
     public Text torchDisplay;
+
+    public int axeCount;
+    public Text axeDisplay;
+
+    public int shovelCount;
+    public Text shovelDisplay;
+
+    public int netCount;
+    public Text netDisplay;
 
     Animator animator;
 
@@ -39,36 +47,28 @@ public class PlayerMovement : MonoBehaviour {
     int currentAnimationState = IDLE;
 
     public bool hasAxe;
-    public bool hasShovel;
     public bool hasNet;
 
-    public int puzzleCounter = 3;
 
-    public GameObject sRune;
-    public GameObject fGem;
-    public GameObject blockTree;
 
     void Start()
     {
-        rigid2D = GetComponent<Rigidbody2D> ();  // set up the Rigidbody2D force 
+        rigid2D = GetComponent<Rigidbody2D>();  // set up the Rigidbody2D force 
 
-        
         logCount = 0;
         clothCount = 0;
         metalCount = 0;
         gemCount = 0;
 
-      bool hasAxe = true;// for testing when inventory is fully implemented this will be set there
-        //bool hasShovel = true;
-      
-        GameObject sRune = GameObject.Find("shovelRune");
-        GameObject fGem = GameObject.Find("finalGem");
-        GameObject blockTree = GameObject.Find("blockTrees");
-        sRune.SetActive(false);
-        fGem.SetActive(false);
+        torchCount = 0;
+        axeCount = 0;
+        shovelCount = 0;
+        netCount = 0;
+
+        bool hasAxe = false;// for testing when inventory is fully implemented this will be set there
+
         animator = GetComponent<Animator>();
     }// end of Start
-
 
     void FixedUpdate()
     {
@@ -76,8 +76,8 @@ public class PlayerMovement : MonoBehaviour {
         float Vertical = Input.GetAxis("Vertical");
         Vector2 move = new Vector2(Horizontal, Vertical);
         rigid2D.AddForce(move * speed);
-        
-        
+
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
 
@@ -99,54 +99,49 @@ public class PlayerMovement : MonoBehaviour {
             changeState(FORWARD_MARCH);
         }
 
-        else if(!Input.anyKey)
+        else if (!Input.anyKey)
         {
-           changeState(IDLE);
-        }
-
-        if(gemCount == 4)
-        {
-            blockTree.SetActive(false);
+            changeState(IDLE);
         }
     }//end of FixedUpdate
 
-        void changeState(int state)
+    void changeState(int state)
+    {
+        if (currentAnimationState == state)
+            return;
+
+        switch (state)
         {
-            if (currentAnimationState == state)
-                return;
+            case IDLE:
+                animator.SetInteger("state", IDLE);
+                break;
 
-            switch(state)
-            {
-                case IDLE:
-                    animator.SetInteger("state", IDLE);
-                    break;
+            case BACKWARDS_MARCH:
+                animator.SetInteger("state", BACKWARDS_MARCH);
+                break;
 
-                case BACKWARDS_MARCH:
-                    animator.SetInteger("state", BACKWARDS_MARCH);
-                    break;
+            case LEFT_MARCH:
+                animator.SetInteger("state", LEFT_MARCH);
+                break;
 
-                case LEFT_MARCH:
-                    animator.SetInteger("state", LEFT_MARCH);
-                    break;
+            case FORWARD_MARCH:
+                animator.SetInteger("state", FORWARD_MARCH);
+                break;
 
-                case FORWARD_MARCH:
-                    animator.SetInteger("state", FORWARD_MARCH);
-                    break;
+            case RIGHT_MARCH:
+                animator.SetInteger("state", RIGHT_MARCH);
+                break;
+        }
 
-                case RIGHT_MARCH:
-                    animator.SetInteger("state", RIGHT_MARCH);
-                    break;
-            }
+        currentAnimationState = state;
 
-            currentAnimationState = state;
+    }//end of changeState
 
-        }//end of changeState
 
     // Upon collision with another trigger game object the script will enter this method.
     // This method will be used to identify the object by tags and preform nessasary actions.
     private void OnTriggerEnter2D(Collider2D other)
     {
- 
         if (other.gameObject.CompareTag("Log"))
         {
             other.gameObject.SetActive(false);// deactivate the object.This will make it dissapear from the game.
@@ -175,16 +170,15 @@ public class PlayerMovement : MonoBehaviour {
             gemDisplay.text = gemCount.ToString();
         }//end if
 
-        if(other.gameObject.CompareTag("chop"))
+        if (other.gameObject.CompareTag("chop"))
         {
-            //hasAxe = true;
             if (hasAxe == true)
             {
                 other.gameObject.SetActive(false);
             }
         }
 
-        if(other.gameObject.CompareTag("SymForFishing") && hasNet == true)
+        if (other.gameObject.CompareTag("SymForFishing") && hasNet == true)
         {
             fishCount = fishCount + 1;
             fishDisplay.text = fishCount.ToString();
@@ -195,190 +189,6 @@ public class PlayerMovement : MonoBehaviour {
             other.gameObject.SetActive(false);
             fishCount -= 1;
         }
-
-
-
-        if (other.gameObject.CompareTag("Fall Tree Puzzle"))
-        {
-            hasAxe = true;
-            if (puzzleCounter == 3 && hasAxe == true)
-            {
-                other.gameObject.SetActive(false);
-                puzzleCounter = puzzleCounter - 1;
-            }
-        }
-
-
-        if (other.gameObject.CompareTag("Spring Tree Puzzle"))
-        {
-            hasAxe = true;
-            if (puzzleCounter == 2 && hasAxe == true)
-            {
-                other.gameObject.SetActive(false);
-                puzzleCounter = puzzleCounter - 1;
-            }
-        }
-
-        if (other.gameObject.CompareTag("Summer Tree Puzzle"))
-        {
-            hasAxe = true;
-            if (puzzleCounter == 1 && hasAxe == true)
-            {
-                sRune.gameObject.SetActive(true);
-                other.gameObject.SetActive(false);
-            
-               
-            }
-        }
-        // summer level "drop" floor challange
-        if (other.gameObject.CompareTag("Drop"))
-        {
-            transform.position = new Vector2(19,-15);
-        }
-
-        
-        // for the trench cover and digging
-        if (other.gameObject.CompareTag("Water Cover1") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover2") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover3") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover4") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover5") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover6") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover7") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover8") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover9") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover10") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover11") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover12") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover13") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover14") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover15") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover16") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover17") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover18") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover19") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover20") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover21") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover22") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover23") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover24") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover25") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover26") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover27") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover28") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover29") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover30") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover31") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover32") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover33") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("Water Cover34") && hasShovel == true)
-        {
-            other.gameObject.SetActive(false);
-            fGem.SetActive(true);
-
-        }
-
-       
-
 
     }// end of OnTriggerEnter
 }// End of PlayerMovement
