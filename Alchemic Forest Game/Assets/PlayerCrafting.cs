d8e702bbe90a5d00ad0bbf2d8c3b806ad1164f1f
Lei Qian <leiqian@nmsu.edu>
2018-05-12 21:31:23 -0600

    05122018 Fixup-Sring-Level
    1. Update the bridge sprite (You can find it in Animations folder)
    
    2. Change the Spotlight type and range (You can change these two back)
    
    3. Update PlayerCrafting.cs (See below):
    switch (bridges)
    {
    case 0:
    GameObject.FindWithTag("Bridge Cover").SetActive(false);
    GameObject.FindWithTag("Bridge Cover1").SetActive(false); // two sprites false together
    break;
    
    case 1:
    GameObject.FindWithTag("Bridge Cover2").SetActive(false);
    GameObject.FindWithTag("Bridge Cover3").SetActive(false); // two sprites false together
    break;
    
    4. Sorry I have no idea of Exit Rune Circle.

--- Alchemic Forest Game/Assets/PlayerCrafting.cs
+++ Alchemic Forest Game/Assets/PlayerCrafting.cs
@@ -48,10 +48,12 @@ public class PlayerCrafting : MonoBehaviour {
                 {
                     case 0:
                         GameObject.FindWithTag("Bridge Cover").SetActive(false);
+                        GameObject.FindWithTag("Bridge Cover1").SetActive(false);
                         break;
 
                     case 1:
                         GameObject.FindWithTag("Bridge Cover2").SetActive(false);
+                        GameObject.FindWithTag("Bridge Cover3").SetActive(false);
                         break;
 
                     case 2:
